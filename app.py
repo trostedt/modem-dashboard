26bde5702d81d2d93bbd55cdefaff06a3d3d3c4f
trostedt <timo.rostedt@gmail.com>
2025-05-13 14:55:27 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -105,8 +105,9 @@ if uploaded_file:
                 fig.add_trace(go.Scatter(
                     x=df[time_col],
                     y=df[f"{modem} - {selected_metric}"],
-                    name=modem,
-                    hovertemplate="Modem: %{name}<br>Value: %{y}<br>Time: %{x} min"
+                    name=f"{modem}",
+                    hovertemplate=f"%{{y}}<br>Time: %{{x}} min",
+                    hovertemplate="%{name}<br>Value: %{y}<br>Time: %{x} min"
                 ))
 
             fig.update_layout(
