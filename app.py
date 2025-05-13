8ee7d1f58b871c348d1129485f6b2bdac34acd9d
trostedt <timo.rostedt@gmail.com>
2025-05-13 16:21:15 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -85,6 +85,7 @@ if uploaded_file:
             )
             fig.update_yaxes(title_text="Disconnect %", secondary_y=False)
             fig.update_yaxes(title_text="Latency / RSSI", secondary_y=True)
+            fig.update_traces(hoverlabel=dict(namelength=-1))
             st.plotly_chart(fig, use_container_width=True)
         else:
             st.warning("Please select at least one metric.")
@@ -110,6 +111,7 @@ if uploaded_file:
                 height=500,
                 hovermode="x unified"
             )
+            fig.update_traces(hoverlabel=dict(namelength=-1))
             st.plotly_chart(fig, use_container_width=True)
 
             if selected_metric == "Disconnect %" and st.checkbox("Show Heatmap"):
